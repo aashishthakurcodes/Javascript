e6b2c7fb0ccc23f6449defa06664fda4303c52f4
Aashish Thakur <j9183208@gmail.com>
2023-01-21 23:29:47 +0530

    Project 5 completed

--- JS01.js/Variable.js
+++ JS01.js/Variable.js
@@ -52,3 +52,10 @@ console.log(str4.includes("Script")); //false
 
 console.log("Question 4 solved");
 
+
+//05. Declare a varibale and assign string value to it and then split it into an array using split() method.
+
+let splt="Aashish Thakur"
+console.log(splt.split(''));
+
+console.log("Question 5 solved");
