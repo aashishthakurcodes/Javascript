a3289d99139e12e040c21a3d9b680c1f74617dbb
Aashish Thakur <j9183208@gmail.com>
2023-01-21 22:59:55 +0530

    Project 1(a) start

---
+++ JS01.js/Variable.js
@@ -0,0 +1,19 @@
+// 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+// String Data type
+let str="Aashish Thakur"
+console.log(str);
+console.log(typeof(str));
+
+
+// Boolean Data type
+let bol=true;
+console.log(typeof(bol));
+
+//Undefined data type
+let undf;
+console.log(typeof(undf));
+
+//Null data type
+let nl= " ";
+console.log(typeof(nl));
