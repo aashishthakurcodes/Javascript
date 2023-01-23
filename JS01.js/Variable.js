5ec0082919b3fae297d39df610a07d2760819f63
Aashish Thakur <j9183208@gmail.com>
2023-01-23 17:14:01 +0530

    Question 10 solved

--- JS01.js/Variable.js
+++ JS01.js/Variable.js
@@ -114,3 +114,34 @@ let var5=895;
 console.log(var5>500);
 console.log(var5>1500);
 
+console.log("Question 9 solved");
+
+// 10. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+//     - 4 > 3 true     w = wrong prediction   c = correct  prediction
+//     - 4 >= 3 false w
+//     - 4 < 3 f c
+//     - 4 <= 3 f c
+//     - 4 == 4 t c
+//     - 4 === 4 t c
+//     - 4 != 4 f c
+//     - 4 !== 4 f c
+//     - 4 != '4' t  w   
+//     - 4 == '4' t c
+//     - 4 === '4' f c
+//     - Find the length of python and jargon and make a falsy comparison statement.
+
+console.log(4 == '4'); //true
+console.log(4 != '4'); //False
+console.log(4 !== 4); //false
+console.log(4 != 4); //false
+console.log(4 === '4'); //false
+console.log(4 >= 4); //true
+
+//Find the length of python and jargon and make a falsy comparison statement.
+
+let pyt="python";
+let jrg="jargon";
+let pyt1=pyt.length
+let jrg1=jrg.length
+console.log(pyt1,jrg1);
+console.log(pyt1 != jrg1);
--- JS01.js/js01.js
+++ JS01.js/js01.js
@@ -2,3 +2,12 @@ console.log("javascript")
 
 console.log("Checking for commmits");
 
+console.log("Hekloo");
+ 
+
+console.log(4 == '4');
+console.log(4 != '4');
+console.log(4 !== 4);
+console.log(4 != 4);
+console.log(4 === '4');
+console.log(4 >= 4);
