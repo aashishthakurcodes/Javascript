51a66a6bec0ec9379b02fff1aad65182b2c14877
Aashish Thakur <j9183208@gmail.com>
2023-01-23 16:39:07 +0530

    Question 9 solved

--- JS01.js/Variable.js
+++ JS01.js/Variable.js
@@ -89,3 +89,28 @@ let val1="   Aashish Thakur "
 let val2=val1.trim();
 console.log(val2);
 console.log(val2.length); //14
+
+
+// 09. Boolean value is either true or false.
+//     - Write three JavaScript statement example which provide truthy value.
+//     - Write three JavaScript statement example which provide falsy value.
+
+//1.
+let var1="Javascript is very powerful language";
+let var2= var1.includes("is")
+console.log(var2); //true
+
+let var3= var1.includes("of")
+console.log(var3); //true
+
+//2.
+let var4="Never Fold Never Break down";
+console.log(var4.startsWith("Never")); // true
+
+console.log(var4.startsWith("DOWN")); // False Exact match.
+
+//3.
+let var5=895;
+console.log(var5>500);
+console.log(var5>1500);
+
