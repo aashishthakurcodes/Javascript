d43b4f765f5d1a464f3df37981dd260fd1cc9ef7
Aashish Thakur <j9183208@gmail.com>
2023-01-23 16:26:20 +0530

    Question 8 solved

--- JS01.js/Variable.js
+++ JS01.js/Variable.js
@@ -81,5 +81,11 @@ console.log(arr2.lastIndexOf('pw skills')); //it return last index value
 
 console.log(arr2.indexOf('pw skills')); // it return first index value
 
+console.log("Question 7 solved");
 
+// 08. Demonstrate the use of trim() to remove any trailing whitespace at the beginning and the end of a string.
 
+let val1="   Aashish Thakur "
+let val2=val1.trim();
+console.log(val2);
+console.log(val2.length); //14
