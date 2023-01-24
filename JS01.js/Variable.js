a4e514fa47d739da2fc617438bff456523253f78
Aashish Thakur <j9183208@gmail.com>
2023-01-24 12:46:06 +0530

    Question 13 solved

--- JS01.js/Variable.js
+++ JS01.js/Variable.js
@@ -205,6 +205,20 @@ console.log(date,"-",month,"-",years, " ",hours,":",minutes);
 //DD/MM/YYYY HH:mm
 console.log(date,"/",month,"/",years, " ",hours,":",minutes);
 
+console.log("Question 12 solved");
+
+
+// 13. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+
+let input=prompt("Enter your age")
+if(input>=18){
+  console.log("You are old enough to drive");
+}
+else{
+  console.log("You are below 18 years");
+}
+
+console.log("Question 13 solved");
 
 
 
