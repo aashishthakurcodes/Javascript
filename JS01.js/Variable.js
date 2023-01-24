42442e7221f46075ba4aa6fe06bf32f808a727c7
Aashish Thakur <j9183208@gmail.com>
2023-01-24 12:38:49 +0530

    Question 12 Solve

--- JS01.js/Variable.js
+++ JS01.js/Variable.js
@@ -184,6 +184,28 @@ console.log(seconds);
 console.log("Question 11 completed");
 
 
+// 12. Create a human readable time format using the Date time object
+//     - YYYY-MM-DD HH:mm
+//     - DD-MM-YYYY HH:mm
+//     - DD/MM/YYYY HH:mm
+
+let dte=new Date()
+ let years=(dte.getFullYear());
+ let month=(dte.getMonth());
+ let date=(dte.getDate());
+ let hours=(dte.getHours());
+ let minutes=(dte.getMinutes());
+
+ //YYYY-MM-DD HH:mm
+  console.log(years,"-",month,"-",date, " ",hours,":",minutes);
+
+//DD-MM-YYYY HH:mm
+console.log(date,"-",month,"-",years, " ",hours,":",minutes);
+
+//DD/MM/YYYY HH:mm
+console.log(date,"/",month,"/",years, " ",hours,":",minutes);
+
+
 
 
 
