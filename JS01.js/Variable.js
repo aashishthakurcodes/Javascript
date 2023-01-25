ebe22cd2b234819249ad78dd3b01b336b13bd49a
Aashish Thakur <j9183208@gmail.com>
2023-01-25 06:54:55 +0530

    Question 18 solved

--- JS01.js/Variable.js
+++ JS01.js/Variable.js
@@ -300,7 +300,22 @@ else{
 }
 console.log("Question 17 solved");
 
+//18. Write a program which tells the number of days in a month, now consider leap year.
 
+let leapyearMnth = prompt("Enter any month name to find the number of total days in a leap year");
+if(leapyearMnth == "January" || leapyearMnth == "March"|| leapyearMnth == "May" || leapyearMnth == "July"|| leapyearMnth == "August"|| leapyearMnth == "October"|| leapyearMnth == "December"){
+  console.log("The totol number of days in this month is 31.");
+}
+else if(leapyearMnth == "April" || leapyearMnth == "June"|| leapyearMnth == "September"|| leapyearMnth == "November"){
+  console.log("The totol number of days in this month is 30.");
+}
+else if(leapyearMnth == "February"){
+  console.log("The totol number of days in this month is 29.");
+}
+else{
+  console.log("Invalid Input");
+}
+console.log("Question 18 solved");
 
 
 
