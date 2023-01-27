1af54634cb8a3f01b7a258f71d572a3bc9543717
Aashish Thakur <j9183208@gmail.com>
2023-01-27 06:14:54 +0530

    DOM project completed

--- DOM project/Project 1/Script.js
+++ DOM project/Project 1/Script.js
@@ -1,38 +1,39 @@
-//Targeting the container Element
- 
+
+// Make a function refresh_btn to refresh the webpage when user click the button 
 function refresh_btn(){
   document.location.reload(true)
 ;}
 
+//Targeting the element
 
+let divElement = document.getElementById("contianer") //it target the container element by its ID
+.addEventListener("click", function (e) {   // Adding Event listner with the click event and a function.
+    let newDiv = document.createElement("div"); //Creating a new div with variable newDiv
+    newDiv.setAttribute("id", "circle"); // Set it's attribute id=circle
+    document.body.appendChild(newDiv);  //Appending newDiv to the page
 
+    let x = e.clientX;  //The clientX read-only property of the MouseEvent interface provides the horizontal coordinate 
 
-let divElement = document
-  .getElementById("contianer")
-  .addEventListener("click", function (e) {
-    let newDiv = document.createElement("div");
-    newDiv.setAttribute("id", "circle");
-    document.body.appendChild(newDiv);
+    let y = e.clientY;  //The clientY read-only property of the MouseEvent interface provides the vertical coordinate e = mouseevent
 
-    let x = e.clientX;
-    let y = e.clientY;
-
-    newDiv.style.top = `${y}px`;
-    newDiv.style.left = `${x}px`;
+    newDiv.style.top = `${y}px`;  //it convert coordinates value into px
+    newDiv.style.left = `${x}px`; //it convert coordinates value into px
 
 
     setTimeout(() => {
       document.body.removeChild(newDiv)
-    }, 5000);
+    }, 5000);  // remove the newDiv after 5 seconds
     
+
+    //This is the function to create randomcolor
     function randomcolor() {
       let hash = "#";
       let color = "123456789ABCDEFG";
       for (let i = 0; i < 6; i++) {
-        hash = hash + color[Math.floor(Math.random() * 16)];
+        hash = hash + color[Math.floor(Math.random() * 16)];  
       }
       return hash;
     }
-     newDiv.style.backgroundColor=randomcolor()
+     newDiv.style.backgroundColor=randomcolor() //Adding to its background
   });
 
