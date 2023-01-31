f4f4aba359545a2f997d073093913d87f9f1caa4
Aashish Thakur <j9183208@gmail.com>
2023-01-31 17:33:09 +0530

    Question 23 solved

--- JS01.js/Question_23/Script_23.js
+++ JS01.js/Question_23/Script_23.js
@@ -1,17 +1,18 @@
 //23. Write a program to check that the number given by the user is a prime number or not.
+// let array=[];
+// let enterNumber= prompt("Enter a number");
+// array.push(enterNumber);
+// console.log(array);
+
+// let prime=array.filter((enterNumber) =>  enterNumber%3!==0 && enterNumber%2!==0 && enterNumber%5!==0 && enterNumber%7!==0 && enterNumber%9!==0 || enterNumber==2 ||enterNumber==3 || enterNumber==5 || enterNumber==7 );
+
+// console.log(prime);
 
 let enterNumber= prompt("Enter a number");
-if (enterNumber === 2) {console.log("This is a prime number");   
-}
-else if(enterNumber<0){
-    console.log("This is a negative number");
-} 
 
-for(let x=2; x<enterNumber; x++){ 
-    if (enterNumber%x === 0) {
-        console.log("This is not a prime number");
-        break;
-    } else {
-        console.log("This is a prime number");
-    }
+if(enterNumber%3!==0 && enterNumber%2!==0 && enterNumber%5!==0 && enterNumber%7!==0 && enterNumber%9!==0 || enterNumber==2 ||enterNumber==3 || enterNumber==5 || enterNumber==7){
+    console.log(`The number ${enterNumber} is a prime number`);
+}
+else{
+    console.log(`The number ${enterNumber} is not a prime number`);
 }
