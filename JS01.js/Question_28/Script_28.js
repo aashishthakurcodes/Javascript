ea1bf44e19431f4057494f256c9fe900595279d2
Aashish Thakur <j9183208@gmail.com>
2023-02-01 20:25:59 +0530

    Question 28 scompleted

--- JS01.js/Question_28/Script_28.js
+++ JS01.js/Question_28/Script_28.js
@@ -13,45 +13,38 @@
 // 	     *
 // 	    ***
 //     *****
+
+
+
 let enterInput=prompt("Press 3 for traingle , 4 for square , 5 for Pyramid")
 
-
-
-// for (let i = 1; i <= 3; i++) {
-//     let row = "";
-//     for (let j = 1; j <= i; j++) {
-//         row += "*";
-//     }
-//     console.log(row);
-// }
-
-// b. Print a square pattern, if the input is 3 then the output should be similar to the given output
-// // 	   ***
-// // 	   ***
-// // 	   ***
-        
-for(let x=1; x<=3; x++){
+if(enterInput==3){
+    for (let i = 1; i <= 3; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+        row += "*";
+    }
+    console.log(row);
+}
+}
+else if(enterInput==4){
+    for(let x=1; x<=3; x++){
     let rw="";
     for(let y=1; y<=3; y++){
         rw+="*";
     }
     console.log(rw);
+}}
 
-}
-
-// 	c. Print a pyramid pattern, if the input is 3 then the output should be similar to the given output
-// 	     *
-// 	    ***
-//     *****
-
-for (let i = 1; i <= 3; i++) {
-    let row = "";
-    for (let j = 1; j <= 3 - i; j++) {
-        row += " ";
+else if(enterInput==5){
+    for (let a = 1; a <= 3; a++) {
+        let row = "";
+        for (let b = 1; b <= 3 - a; b++) {
+            row += " ";
+        }
+        for (let c = 1; c <= 2 * a - 1; c++) {
+            row += "*";
+        }
+        console.log(row);
     }
-    for (let k = 1; k <= 2 * i - 1; k++) {
-        row += "*";
-    }
-    console.log(row);
 }
-         
