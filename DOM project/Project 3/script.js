eead314fad7e6a5bfd1dffa6ebd13a68e2c8ea22
Aashish Thakur <j9183208@gmail.com>
2023-02-12 22:14:12 +0530

    Adding styling

--- DOM project/Project 3/script.js
+++ DOM project/Project 3/script.js
@@ -14,14 +14,12 @@ button.addEventListener('click',function(){
   newelement.appendChild(para);
 
   let time=new Date();
-  
   newelement.append(time.toLocaleTimeString())
   
 
   
-  
   let rmvbtn = document.createElement("button");
-  rmvbtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/> </svg>';
+  rmvbtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18"  viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M175 175C184.4 165.7 199.6 165.7 208.1 175L255.1 222.1L303 175C312.4 165.7 327.6 165.7 336.1 175C346.3 184.4 346.3 199.6 336.1 208.1L289.9 255.1L336.1 303C346.3 312.4 346.3 327.6 336.1 336.1C327.6 346.3 312.4 346.3 303 336.1L255.1 289.9L208.1 336.1C199.6 346.3 184.4 346.3 175 336.1C165.7 327.6 165.7 312.4 175 303L222.1 255.1L175 208.1C165.7 199.6 165.7 184.4 175 175V175zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>';
   rmvbtn.setAttribute('id',"rmvbutton");
 
   newelement.appendChild(rmvbtn)
